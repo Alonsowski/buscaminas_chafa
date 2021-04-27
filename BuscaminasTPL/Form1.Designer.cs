ecbb9c4ce681ff053fb0d29cbffe2a28e462be07
Alonsowski <santanasamuel29@gmail.com>
2021-04-26 22:46:21 -0500

    primer commit

---
+++ BuscaminasTPL/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace BuscaminasTPL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(415, 22);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Tag = "";
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(309, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 399);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Buscaminas TPL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BuscaminasTPL/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaminasTPL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void GenerateButtonArrays(int num)
+        {
+            int mv = 5, mh = 0;
+            for (int i = 0; i < num; i++)
+            {
+                mh = 5;
+                for (int j = 0; j < num; j++)
+                {
+                    Button btn = new Button();
+                    btn.Height = 24;
+                    btn.Width = 24;
+                    btn.Top = mv;
+                    btn.Left = mh;
+                    char mn = cmatriz[i, j];
+                    btn.Tag = i.ToString() + "," + j.ToString();
+                    //btn.Text = matriz[i, j].ToString();
+                    btn.Click += (s, ev) =>
+                    {
+                        //int a = int.Parse(btn.Tag.ToString().Split(',')[0]), b = int.Parse(btn.Tag.ToString().Split(',')[1]);
+                        //if (mn.Equals('0'))
+                        //    findLimits(a, b, num);
+                        //else
+                            ShowValue(btn, mn);
+                    };
+                    panel1.Controls.Add(btn);
+                    mh += 24;
+                    bmatriz[i, j] = btn;
+                }
+                mv += 24;
+            }
+        }
+        public void ShowValue(Button b, char value)
+        {
+            b.Text = value.ToString();
+        }
+        public void findLimits(int i, int j, int num)
+        {
+            ShowValue(bmatriz[i, j], cmatriz[i, j]);
+            if (j + 1 < num)
+            {
+                if (cmatriz[i, j + 1].Equals('0'))
+                    findLimits(i, j + 1, num);
+                if (i + 1 < num && cmatriz[i + 1, j + 1].Equals('0'))
+                    findLimits(i + 1, j + 1, num);
+                if (i > 0 && cmatriz[i - 1, j + 1].Equals('0'))
+                    findLimits(i - 1, j + 1, num);
+            }
+            if (j > 0)
+            {
+                if (cmatriz[i, j - 1].Equals('0'))
+                    findLimits(i, j - 1, num);
+                if (i + 1 < num && cmatriz[i + 1, j - 1].Equals('0'))
+                    findLimits(i + 1, j - 1, num);
+                if (i > 0 && cmatriz[i - 1, j - 1].Equals('0'))
+                    findLimits(i - 1, j - 1, num);
+            }
+            if (i + 1 < num && cmatriz[i + 1, j].Equals('0'))
+                findLimits(i + 1, j, num);
+            if (i > 0 && cmatriz[i - 1, j].Equals('0'))
+                findLimits(i - 1, j, num);
+        }
+        static ReaderWriterLockSlim padlock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+
+        public int[,] matriz;
+        public Char[,] cmatriz;
+        public Button[,] bmatriz;
+        private async void btnGenerar_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            int num = int.Parse(this.textBox1.Text);
+            bmatriz = new Button[num, num];
+            cmatriz = await Task.Run(() =>
+                {
+                    padlock.EnterReadLock();
+                    int n = num;
+                    char[,] m = new char[n, n];
+                    padlock.ExitReadLock();
+                    Console.WriteLine(n);
+                    Random r = new Random();
+                    for (int i = 0; i < num; i++)
+                    {
+                        for (int j = 0; j < num; j++)
+                        {
+                            if(r.Next(10) == 1)
+                            {
+                                m[i, j] = '*';
+                            }
+                        }
+                    }
+                    //padlock.EnterWriteLock();
+                    //matriz = m;
+                    //padlock.ExitWriteLock();
+                    return m;
+                }
+            );
+            cmatriz = await Task.Run(() =>
+            {
+                padlock.EnterReadLock();
+                int n = num;
+                char[,] m = cmatriz;
+                padlock.ExitReadLock();
+                for (int i = 0; i < num; i++)
+                {
+                    for (int j = 0; j < num; j++)
+                    {
+                        if (!m[i, j].Equals('*'))
+                        {
+                            int val = 0;
+                            if (j + 1 < num)
+                            {
+                                if (m[i, j + 1].Equals('*'))
+                                    val++;
+                                if (i + 1 < num && m[i + 1, j + 1].Equals('*'))
+                                    val++;
+                                if (i > 0 && m[i - 1, j + 1].Equals('*'))
+                                    val++;
+                            }
+                            if (j - 1 >= 0)
+                            {
+                                if (m[i, j - 1].Equals('*'))
+                                    val++;
+                                if (i + 1 < num && m[i + 1, j - 1].Equals('*'))
+                                    val++;
+                                if (i > 0 && m[i - 1, j - 1].Equals('*'))
+                                    val++;
+                            }
+                            if (i + 1 < num && m[i + 1, j].Equals('*'))
+                                val++;
+                            if (i > 0 && m[i - 1, j].Equals('*'))
+                                val++;
+                            m[i, j] = Convert.ToChar(val+48);
+                        }
+                        Console.WriteLine(m[i, j]);
+                    }
+                }
+                return m;
+            });
+            GenerateButtonArrays(num);
+            
+        }
+    }
+}
